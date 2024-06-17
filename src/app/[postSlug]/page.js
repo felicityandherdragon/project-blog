5a499d7825a30e6ce16dbdf17fb1edeb1c0a0960
felicityandherdragon <felicitywuny@gmail.com>
2024-06-16 21:40:08 -0400

    save progress

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { MDXRemote } from 'next-mdx-remote/rsc';
+import CodeSnippet from '@/components/CodeSnippet';
+import DivisionGroupsDemo from '@/components/DivisionGroupsDemo';
 
 import { loadBlogPost } from '@/helpers/file-helpers';
 
 import BlogHero from '@/components/BlogHero';
 
 import styles from './postSlug.module.css';
+
+export async function generateMetadata({ params }) {
+  const content = await loadBlogPost(params.postSlug);
+
+  return {
+    title: content.frontmatter.title,
+    description: content.frontmatter.abstract,
+  };
+}
 
 async function BlogPost({ params }) {
   const content = await loadBlogPost(params.postSlug);
@@ -17,7 +28,13 @@ async function BlogPost({ params }) {
         publishedOn={content.frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={content.content} />
+        <MDXRemote
+          source={content.content}
+          components={{
+            pre: CodeSnippet,
+            DivisionGroupsDemo,
+          }}
+        />
       </div>
     </article>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,13 @@ import BlogSummaryCard from '@/components/BlogSummaryCard';
 
 import styles from './homepage.module.css';
 
+import { BLOG_TITLE } from '@/constants';
+
+export const metadata = {
+  title: BLOG_TITLE,
+  description: 'A wonderful blog about JavaScript',
+};
+
 async function Home() {
   const allBlogPosts = await getBlogPostList();
 
